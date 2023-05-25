44649e8623ef0adf95b614e0ab8f58de5a73fb9c
tsionassefa <61678984+tsionassefa@users.noreply.github.com>
2023-05-25 21:26:50 +0300

    Update db_storage.py

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -16,8 +16,7 @@ import sqlalchemy
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 
-classes = {"Amenity": Amenity, "City": City,
-           "Place": Place, "Review": Review, "State": State, "User": User}
+classes = {"Amenity": Amenity, "City": City, "Review": Review, "State": State, "User": User}
 
 
 class DBStorage:
