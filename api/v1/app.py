18c6789503a32ddbb52c2c140769601a2dce938a
Mulubrhan1511 <mulubrhangebrkidan@gmail.com>
2023-05-27 12:33:35 +0300

    mule

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,3 +1,9 @@
+#!/usr/bin/python3
+"""
+Fabric script that generates a tgz archive from the contents of the web_static
+folder of the AirBnB Clone repo
+"""
+
 from flask import Flask
 from models import storage
 from api.v1.views import app_views
--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,3 +1,9 @@
+#!/usr/bin/python3
+"""
+Fabric script that generates a tgz archive from the contents of the web_static
+folder of the AirBnB Clone repo
+"""
+
 from flask import Blueprint
 from api.v1.views.index import *
 
