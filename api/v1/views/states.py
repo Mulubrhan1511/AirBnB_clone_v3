086ebd3bd956f38a922255f357ce3575787908e3
Mulubrhan1511 <mulubrhangebrkidan@gmail.com>
2023-05-27 13:47:29 +0300

    mule

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,3 +1,9 @@
+#!/usr/bin/python3
+"""
+Fabric script that generates a tgz archive from the contents of the web_static
+folder of the AirBnB Clone repo
+"""
+
 from models import storage
 from api.v1.views import app_views
 from flask import jsonify, abort, request
