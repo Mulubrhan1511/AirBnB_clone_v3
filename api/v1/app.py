aa57cd626cd233addb0a0a2bcc1be03e48b73e92
Mulubrhan1511 <mulubrhangebrkidan@gmail.com>
2023-05-27 14:15:45 +0300

    mule

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,21 +1,23 @@
 #!/usr/bin/python3
 """
-Fabric script that generates a tgz archive from the contents of the web_static
-folder of the AirBnB Clone repo
+This module contains the principal application
 """
-
-from flask import Flask, make_response, jsonify
 from models import storage
 from api.v1.views import app_views
+from flask import Flask, make_response, jsonify
 from os import getenv
+from flask_cors import CORS
+from flasgger import Swagger
 
 app = Flask(__name__)
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 app.register_blueprint(app_views)
+cors = CORS(app, resources={r"/api/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
-def close_storage(exception):
+def close_db(obj):
+    """ calls methods close() """
     storage.close()
 
 
@@ -25,7 +27,17 @@ def page_not_foun(error):
     return make_response(jsonify({"error": "Not found"}), 404)
 
 
+app.config['SWAGGER'] = {
+    'title': 'AirBnB clone - RESTful API',
+    'description': 'This is the api that was created for the hbnb restful api project,\
+    all the documentation will be shown below',
+    'uiversion': 3}
+
+Swagger(app)
+
 if __name__ == "__main__":
+
     host = getenv('HBNB_API_HOST', default='0.0.0.0')
     port = getenv('HBNB_API_PORT', default=5000)
+
     app.run(host, int(port), threaded=True)
