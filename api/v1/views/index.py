b51b41ee2c315fddcb1cc0a2aa149ee84d059539
Mulubrhan1511 <mulubrhangebrkidan@gmail.com>
2023-05-27 13:00:40 +0300

    mule

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -17,7 +17,9 @@ def get_status():
 
 @app_views.route("/stats", strict_slashes=False)
 def get_stats():
-    stats = {}
-    for cls in storage.classes:
-        stats[cls] = storage.count(cls)
-    return jsonify(stats)
+    return jsonify({"amenities": storage.count("Amenity"),
+                    "cities": storage.count("City"),
+                    "places": storage.count("Place"),
+                    "reviews": storage.count("Review"),
+                    "states": storage.count("State"),
+                    "users": storage.count("User")})
