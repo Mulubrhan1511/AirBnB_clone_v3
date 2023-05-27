3f89a124a3d5529b7e025cbb4a26c8d43c6e88a8
Mulubrhan1511 <mulubrhangebrkidan@gmail.com>
2023-05-27 12:36:13 +0300

    mule

--- api/v1/app.py
+++ api/v1/app.py
@@ -7,6 +7,7 @@ folder of the AirBnB Clone repo
 from flask import Flask
 from models import storage
 from api.v1.views import app_views
+from os import getenv
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
@@ -18,7 +19,6 @@ def close_storage(exception):
 
 
 if __name__ == "__main__":
-    import os
-    host = os.getenv("HBNB_API_HOST", "0.0.0.0")
-    port = int(os.getenv("HBNB_API_PORT", 5000))
+    host = getenv('HBNB_API_HOST', default='0.0.0.0')
+    port = getenv('HBNB_API_PORT', default=5000)
     app.run(host=host, port=port, threaded=True)
